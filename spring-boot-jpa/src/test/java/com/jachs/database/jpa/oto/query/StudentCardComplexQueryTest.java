ea7a9ec5b2d30f8b8147649e74860c4afc470471
'jachs' <799516423@qq.com>
2021-06-03 18:00:30 +0800

    ACZ

--- spring-boot-jpa/src/test/java/com/jachs/database/jpa/oto/query/StudentCardComplexQueryTest.java
+++ spring-boot-jpa/src/test/java/com/jachs/database/jpa/oto/query/StudentCardComplexQueryTest.java
@@ -1,5 +1,7 @@
 package com.jachs.database.jpa.oto.query;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,17 +18,27 @@ import com.jachs.database.jpa.entity.oto.StudentCard;
 public class StudentCardComplexQueryTest {
 	@Autowired
 	private StudentCardRepository studentCardRepository;
-	
-	//单字段查询 
+
+	// 单字段查询
 	@Test
 	public void test() {
-		StudentCard sc=studentCardRepository.findBycardName("机随称随");
+		StudentCard sc = studentCardRepository.findBycardName("机随称随");
 		System.out.println(sc.getCardId());
 	}
-	//多条件And
+
+	// 多条件And
 	@Test
 	public void test1() {
-		StudentCard sc=studentCardRepository.findByCardNameAndCardType("机随称随","学生卡1");
+		StudentCard sc = studentCardRepository.findByCardNameAndCardType("机随称随", "学生卡1");
 		System.out.println(sc.getCardId());
+	}
+
+	@Test
+	public void test2() {
+		List<StudentCard> scList = studentCardRepository.findByCardType("学生卡1");
+		
+		for (StudentCard studentCard : scList) {
+			System.out.println(studentCard.getCardId());
+		}
 	}
 }
