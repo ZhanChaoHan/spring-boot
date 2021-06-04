20e3f8941a825e013466fcba5fa0b90c5b7f74ad
'jachs' <799516423@qq.com>
2021-06-04 10:09:45 +0800

    QAZ

--- spring-boot-jpa/src/test/java/com/jachs/database/jpa/oto/query/StudentCardComplexQueryTest.java
+++ spring-boot-jpa/src/test/java/com/jachs/database/jpa/oto/query/StudentCardComplexQueryTest.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.jachs.database.jpa.dao.oto.StudentCardRepository;
 import com.jachs.database.jpa.entity.oto.StudentCard;
@@ -36,9 +40,50 @@ public class StudentCardComplexQueryTest {
 	@Test
 	public void test2() {
 		List<StudentCard> scList = studentCardRepository.findByCardType("学生卡1");
-		
+
 		for (StudentCard studentCard : scList) {
 			System.out.println(studentCard.getCardId());
+		}
+	}
+	//匿名占位符
+	@Test
+	public void test5() {
+		List<StudentCard> scList = studentCardRepository.findByCardNameOrCardType("机随称随", "学生卡1");
+		for (StudentCard studentCard : scList) {
+			System.out.println(studentCard.getCardName()+"\t\t"+studentCard.getCardType());
+		}
+	}
+	//排序
+	@Test
+	public void test3() {
+		//默认升序
+//		List<StudentCard> scList = studentCardRepository.findByAndSort("学生卡", Sort.by("cardId"));
+//		for (StudentCard studentCard : scList) {
+//			System.out.println(studentCard.getCardId());
+//		}
+		//改为降序
+		List<StudentCard> scList = studentCardRepository.findByAndSort("学生卡", Sort.by("cardId").descending());
+		for (StudentCard studentCard : scList) {
+			System.out.println(studentCard.getCardId());
+		}
+	}
+	//分页
+	@Test
+	public void test6() {
+		//参数1:页码，参数2：每页条数，参数3：排序字段以及排序方式
+		Pageable pageable=PageRequest.of(1, 2, Sort.by("cardMonery"));
+		Page<StudentCard> scList=studentCardRepository.findAll(pageable);
+		for (StudentCard studentCard : scList) {
+			System.out.println(studentCard.getCardId()+"\t\t"+studentCard.getCardMonery());
+		}
+	}
+	//条件分页
+	@Test
+	public void test7() {
+		Pageable pageable=PageRequest.of(0, 2, Sort.by("cardMonery"));
+		Page<StudentCard> scList=studentCardRepository.findByCardType("学生卡1",pageable);
+		for (StudentCard studentCard : scList) {
+			System.out.println(studentCard.getCardId()+"\t\t"+studentCard.getCardType()+"\t\t"+studentCard.getCardMonery());
 		}
 	}
 }
