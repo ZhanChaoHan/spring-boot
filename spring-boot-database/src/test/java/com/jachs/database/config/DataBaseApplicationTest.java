a872c681f1cf8037ab537e387ef9338f35eef378
'jachs' <799516423@qq.com>
2021-06-04 17:51:04 +0800

    axzz

--- spring-boot-database/src/test/java/com/jachs/database/config/DataBaseApplicationTest.java
+++ spring-boot-database/src/test/java/com/jachs/database/config/DataBaseApplicationTest.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
