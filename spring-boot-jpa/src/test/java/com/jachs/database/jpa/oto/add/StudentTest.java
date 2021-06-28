71bde294934d54062b60551237db1841e52a3fbe
“我听别人说，这世界上有一种电脑是没有关机键的，它只能够一直的运行呀运行呀，运行累了就在数据流里面睡一会，这种电脑一辈子只能关机一次，那一次就是它宕机的时候。” ——《阿机正传》 <799516423@qq.com>
2021-06-28 15:03:25 +0800

    Update StudentTest.java

--- spring-boot-jpa/src/test/java/com/jachs/database/jpa/oto/add/StudentTest.java
+++ spring-boot-jpa/src/test/java/com/jachs/database/jpa/oto/add/StudentTest.java
@@ -1,5 +1,12 @@
 package com.jachs.database.jpa.oto.add;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,39 +21,61 @@ import com.jachs.database.jpa.entity.oto.StudentCard;
  */
 @SpringBootTest
 public class StudentTest {
-    @Autowired
-    private StudentRepository studentRepository;
-    
-    //单存
-    @Test
-    public void saveAStudent() {
-        Student s=new Student();
-        s.setStudentId ( "sOne" );
-        s.setStudentName ( "马保国" );
-        s.setStudentAge ( 69 );
-        
-        studentRepository.save ( s );
-    }
-    //关联存储
-    @Test
-    public void saveAStudentAndCard() {
-        Student s=new Student();
-        s.setStudentId ( "sOne" );
-        s.setStudentName ( "马保国" );
-        s.setStudentAge ( 69 );
-        
-        StudentCard sc=new StudentCard();
-        sc.setCardId ( "sOneC" );
-        sc.setCardMonery ( 5L );
-        sc.setCardType ( "老年卡" );
-        
-        
-        s.setStudentCard ( sc );
-        studentRepository.save ( s );
-    }
-    //删除主表从表数据一起删了
-    @Test
-    public void deleteStuend() {
-        studentRepository.delete ( studentRepository.findById ( "sOne" ).get () );
-    }
+	@Autowired
+	private StudentRepository studentRepository;
+
+	// 单存
+	@Test
+	public void saveAStudent() {
+		Student s = new Student();
+		s.setStudentId("sOne");
+		s.setStudentName("马保国");
+		s.setStudentAge(69);
+		s.setDateOfBirth(new Date());
+		studentRepository.save(s);
+	}
+
+	// 关联存储
+	@Test
+	public void saveAStudentAndCard() {
+		Student s = new Student();
+		s.setStudentId("sOne");
+		s.setStudentName("马保国");
+		s.setStudentAge(69);
+		s.setDateOfBirth(new Date());
+
+		StudentCard sc = new StudentCard();
+		sc.setCardId("sOneC");
+		sc.setCardMonery(5L);
+		sc.setCardType("老年卡");
+
+		s.setStudentCard(sc);
+		studentRepository.save(s);
+	}
+
+	@Test
+	public void saveMany() {
+		List<Student>allS=new ArrayList<Student>();
+		RandomStringUtils rsu=new RandomStringUtils();
+		Random rd=new Random();
+		DateUtils du=new DateUtils();
+		Date da=new Date();
+		for (int kk = 0; kk < 100; kk++) {
+			Student s = new Student();
+			
+			s.setStudentId(rsu.randomAlphabetic(255));
+			s.setStudentName(rsu.randomAlphabetic(255));
+			s.setStudentAge(rd.nextInt());
+			s.setDateOfBirth(du.addDays(da, rd.nextInt(50)));
+			
+			allS.add(s);
+		}
+		studentRepository.saveAll(allS);
+	}
+
+	// 删除主表从表数据一起删了
+	@Test
+	public void deleteStuend() {
+		studentRepository.delete(studentRepository.findById("sOne").get());
+	}
 }
